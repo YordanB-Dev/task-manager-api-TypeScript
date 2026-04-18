70a875831ad0371e8f10d8487a5086df052483be
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-04-18 12:15:21 +0200

    Refactor server.ts to use AppError for error handling

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
-import app from "./app.js";
 
 dotenv.config();
+
+import app from "./app.js";
+import { AppError } from "./middleware/types/AppError.js";
 
 const portEnv = process.env.PORT;
 const PORT = portEnv ? Number(portEnv) : 3000;
 
-if (Number.isNaN(PORT) || PORT <= 0) {
-    throw new Error(`Invalid PORT value. Set process.env.PORT to a positive number.`);
+if(Number.isNaN(PORT) || PORT <= 0) {
+    throw new AppError("Invalid PORT value. SET process.env.PORT to a positive number", 400);
 };
 
 app.listen(PORT, () => {
