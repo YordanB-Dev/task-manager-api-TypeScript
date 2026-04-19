0ec7265c3361599272a3fc77a33ab9a8716405d8
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-04-19 12:03:36 +0200

    Add auth middleware to get all tasks route
    Added auth middleware to the getAllTask route.

--- routes.ts
+++ routes.ts
@@ -6,17 +6,17 @@ import validateTask from './middleware/validateTask.js';
 
 const router = express.Router();
 
-// AUTH
+
 router.post('/auth/register', userController.register);
 router.post('/auth/login', userController.login);
 
-// PROTECTED TASK ROUTES
+
 router.post('/task', authMiddleware, validateTask, taskController.createTask);
 router.put('/task/:id', authMiddleware, validateTask, taskController.updateTask);
 router.delete('/task/:id', authMiddleware, taskController.deleteTask);
 
-// PUBLIC
-router.get('/task', taskController.getAllTask);
+
+router.get('/task', authMiddleware, taskController.getAllTask);
 router.get('/task/:id', taskController.getTaskById);
 
 export default router;
