86c137db1b9c7f6769febcf0bae3c9f189996ecf
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-04-20 11:26:49 +0200

    Add username to registration and login handlers

--- user.controller.ts
+++ user.controller.ts
@@ -5,25 +5,26 @@ import userService from "../services/user.service.js";
 interface AuthBody {
     email: string,
     password: string;
+    username: string
 }
 
 export const register = asyncHandler (
     async (req: Request, res: Response) => {
-        const {email, password} = req.body as AuthBody;
+        const {email, password, username} = req.body as AuthBody;
 
-        const result = await userService.register(email, password);
+        const result = await userService.register(email, password, username);
 
-        res.status(201).json(result);
+        return res.status(201).json(result);
     }
 );
 
 export const login = asyncHandler (
     async (req: Request, res: Response) => {
-        const {email, password} = req.body as AuthBody;
+        const { email, password, username } = req.body as AuthBody;
 
-        const user = await userService.login(email, password);
+        const user = await userService.login(email, password, username);
 
-        res.status(200).json(user);
+        return res.status(200).json(user);
     }
 );
 
