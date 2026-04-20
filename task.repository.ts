b33656999837b621269527abe44385b061d2a029
Yordan Borisov <67415791+YordanB-Dev@users.noreply.github.com>
2026-04-20 11:30:28 +0200

    Enhance getAllTasks with search and completed filters
    Added search and completed filter to getAllTasks function.

--- task.repository.ts
+++ task.repository.ts
@@ -1,13 +1,35 @@
 import db from "../db.js";
 import type { QueryResult } from "pg";
 
-export const getAllTask = async (userId: number): Promise<QueryResult> => {
-    return db.query(`SELECT * FROM task WHERE userId = $1`, [userId]);
+const getAllTasks = async (
+    userId: number,
+    search?: string,
+    completed?: string
+) => {
+
+    let query = `SELECT * FROM task WHERE "userId" = $1`;
+    const values: any[] = [userId];
+    if (search && search.trim() !== "") {
+        values.push(`%${search}%`);
+        query += ` AND title ILIKE $${values.length}`;
+    }
+
+    if (completed !== undefined) {
+        values.push(completed === "true");
+        query += ` AND completed = $${values.length}`;
+    }
+
+    query += ` ORDER BY created_at DESC`;
+
+    console.log(query);
+    console.log(values);
+
+    return db.query(query, values);
 };
 
 export const getTaskById = async (id: number, userId: number): Promise<QueryResult> => {
     return db.query(
-        `SELECT * FROM task WHERE id = $1 AND userId = $2`,
+        `SELECT * FROM task WHERE id = $1 AND "userId" = $2`,
         [id, userId]
     );
 };
@@ -18,7 +40,7 @@ export const createTask = async (
     userId: number
 ): Promise<QueryResult> => {
     return db.query(
-        `INSERT INTO task (title, description, userId) VALUES ($1, $2, $3) RETURNING *`,
+        `INSERT INTO task (title, description, "userId") VALUES ($1, $2, $3) RETURNING *`,
         [title, description, userId]
     );
 };
@@ -30,20 +52,20 @@ export const updateTask = async (
     userId: number
 ): Promise<QueryResult> => {
     return db.query(
-        `UPDATE task SET title = $1, description = $2 WHERE id = $3 AND userId = $4 RETURNING *`,
+        `UPDATE task SET title = $1, description = $2 WHERE id = $3 AND "userId" = $4 RETURNING *`,
         [title, description, id, userId]
     );
 };
 
 export const deleteTask = async (id: number, userId: number): Promise<QueryResult> => {
     return db.query(
-        `DELETE FROM task WHERE id = $1 AND userId = $2 RETURNING *`,
+        `DELETE FROM task WHERE id = $1 AND "userId" = $2 RETURNING *`,
         [id, userId]
     );
 };
 
 export default {
-    getAllTask,
+    getAllTasks,
     getTaskById,
     createTask,
     updateTask,
